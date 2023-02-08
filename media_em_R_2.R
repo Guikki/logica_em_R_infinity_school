8ea3300583aa8a2b3c8d3535722177e284862ee5
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-08 20:09:15 -0300

    Create media_em_R_2.R

---
+++ media_em_R_2.R
@@ -0,0 +1,10 @@
+print(' faça o input da 1a nota ')
+#av1<-scan()
+av1<-as.numeric(readline(prompt="Insira a primeira nota: "))
+print(' faça o input da 2a nota ')
+#av2<-scan()
+av2<-as.numeric(readline(prompt="Insira a segunda nota: "))
+
+media<-(av1+av2)/2
+print(media)
+print(paste("a nota média foi igual a:",media))
