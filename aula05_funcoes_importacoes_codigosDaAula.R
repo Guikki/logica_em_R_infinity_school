8d12d46f4b10040f57c778c2bb0e2233dfee299b
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-03-15 21:15:22 -0300

    Create aula05_funcoes_importacoes_codigosDaAula.R

---
+++ aula05_funcoes_importacoes_codigosDaAula.R
@@ -0,0 +1,24 @@
+# print("teste")
+# arq1 <- read.table(file = "teste.csv", sep=",", header=TRUE)
+# print(head(arq1))
+
+#  arq2 <- read.table(file = "C:/Users/aluno.3D/Documents/teste2.csv")
+#  print(head(arq2))
+
+#  x <- "texto em R"
+#  write.table(x, "file1.csv")
+
+#  install.packages("readxl")
+
+# library(readxl)
+# # # # dados1 <- read_csv(file = "teste.csv")
+# # # # print(head(dados1))
+
+# dados2 <- read_xls("dados1.xls")
+# print(head(dados2))
+
+# install.packages("curl")
+
+# library(curl)
+# arq <- read.csv(curl("https://raw.githubusercontent.com/dourivaljunior/Curso-de-Python/main/arquivo_dados_funcionario.csv"))
+# print(head(arq))
